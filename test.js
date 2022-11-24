e07c62465c12b29af75644169d9f3a74ed7b0989
BORRAM <BORRAM>
2022-11-25 01:41:28 +0300

    TEST0141

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 //cemberin çapını verip çevresini hesaplayan program
-var r = 5; 
+var r = 5;
 var pi = 3.14;
 var cevre = 2 * pi * r;
 console.log(cevre);
 console.log("test");
+console.log("test2");
